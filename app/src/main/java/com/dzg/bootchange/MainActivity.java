0c0544f53a5d2ef143fbcd3c26bfd8a3cd7a8947
dengzhouguang <979611780@qq.com>
2017-09-05 12:51:29 +0800

    new version

--- app/src/main/java/com/dzg/bootchange/MainActivity.java
+++ app/src/main/java/com/dzg/bootchange/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.dzg.bootchange.bean.StepBean;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -16,8 +18,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
@@ -30,36 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.id_tv);
-        mEditText = (EditText) findViewById(R.id.id_et);
-        mButton = (Button) findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!isRoot){
-                    Toast.makeText(MainActivity.this,"没有ROOT权限，请先授予本应用ROOT权限",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                putToSP();
-                save();
-                Toast.makeText(MainActivity.this,"修改成功",Toast.LENGTH_SHORT).show();
-            }
-        });
-        getBoot(false);
-        showEdit();
+        getBoot();
+        editSP();
+        save();
+        finish();
     }
 
     private void save() {
         try {
-            File file=new File("/data/data/com.dzg.bootchange/shared_prefs/NewPedoMeter.xml");
+            File file=new File("/data/data/com.dzg.bootchange/shared_prefs/NewPedoMeter_private.xml");
             FileInputStream fis=new FileInputStream(file);
-            byte[] bs=new byte[2*1024*1024];
+            byte[] bs=new byte[8*1024*1024];
             int len=-1;
-            try {
-                len=fis.read(bs);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+            len = fis.read(bs);
             String sp=new String(bs,0,len,"utf-8").replace("\"","\\\"");
             Exec(sp);
         } catch (Exception e) {
@@ -68,44 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void showEdit() {
-        if (!isRoot){
-            mTextView.setText("尚未安装支付宝或没有ROOT权限");
-            return;
-        }
-        SharedPreferences sp =getSharedPreferences("NewPedoMeter", Context.MODE_PRIVATE);
-        String boot=sp.getString("last_stepinfo_today","0");
-        String baseStep=sp.getString("baseStep","0");
-        if (!boot.equals("0"))
-        boot=boot.substring(boot.indexOf("steps\":")+"steps\":".length(),boot.lastIndexOf(","));
-        if (!baseStep.equals("0"))
-        baseStep=baseStep.substring(baseStep.indexOf(":")+1,baseStep.indexOf(","));
-        if (boot.contains(";")){
-            mTextView.setText("总步数：0");
-        }else
-        mTextView.setText("总步数："+boot);
-        mEditText.setText(baseStep);
-        mEditText.setSelection(baseStep.length());
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
-        getBoot(true);
-        showEdit();
     }
 
-    public void getBoot(boolean reload){
+    public void getBoot(){
         Process process = null;
         DataOutputStream os = null;
-        DataInputStream is=null;
+        DataInputStream is;
         File dir=new File("/data/data/com.dzg.bootchange/shared_prefs/");
         if (!dir.exists()){
             dir.mkdirs();
         }
         String cmd="chmod 777 " + PCKAGE_NAME;
-        String editString="cat /data/data/com.eg.android.AlipayGphone/shared_prefs/NewPedoMeter.xml";
-
+        String editString="cat /data/data/com.eg.android.AlipayGphone/shared_prefs/NewPedoMeter_private.xml";
         try {
             process = Runtime.getRuntime().exec("su"); //切换到root帐号
             os = new DataOutputStream(process.getOutputStream());
@@ -114,75 +78,54 @@ public class MainActivity extends AppCompatActivity {
             os.writeBytes(editString+"\n");
             os.writeBytes("exit\n");
             os.flush();
-            byte[] bs=new byte[2*1024*1024];
+            byte[] bs=new byte[8*1024*1024];
             int len=-1;
             len=is.read(bs);
-            if (!reload){
-            File file=new File("/data/data/com.dzg.bootchange/shared_prefs/NewPedoMeter.xml");
+            File file=new File("/data/data/com.dzg.bootchange/shared_prefs/NewPedoMeter_private.xml");
             FileWriter writer=new FileWriter(file);
             if(len>0){
             writer.write(new String(bs,0,len-1,"utf-8"));}
             writer.flush();
             writer.close();
-            }else {
-                if (len==-1){
-                    isRoot=false;
-                    return;
-                }else
-                    isRoot=true;
-                String str=new String(bs,0,len,"utf-8");
-                String startCount=str.substring(str.indexOf("{&quot;steps&quot;:")+"{&quot;steps&quot;:".length());
-                startCount=startCount.substring(0,startCount.indexOf(","));
-                String count=str.substring(str.indexOf(",&quot;steps&quot;:")+",&quot;steps&quot;:".length());
-                count=count.substring(0,count.indexOf(","));
-                SharedPreferences sp =getSharedPreferences("NewPedoMeter", Context.MODE_PRIVATE);
-                String boot=sp.getString("last_stepinfo_today","");
-                String userId=sp.getString("userId","0");
-                String head="";
-                String end="";
-                if (!boot.equals("")) {
-                    head= boot.substring(0, boot.indexOf("steps\":") + "steps\":".length());
-                    end= boot.substring(boot.lastIndexOf(","), boot.length());
-                }else
-                {
-                    head="{&quot;biz&quot;:&quot;alipay&quot;,&quot;steps&quot;:";
-                    end=",&quot;time&quot;:"+ new Date().getTime()+"}";
-                }
-                String baseStep=sp.getString("baseStep","");
-                String baseHead=null;
-                String baseEnd=null;
-                if(!baseStep.equals("")) {
-                    baseHead= baseStep.substring(0, baseStep.indexOf(":") + 1);
-                    baseEnd= baseStep.substring(baseStep.indexOf(","), baseStep.length());
-                }else{
-                    baseHead=">{&quot;steps&quot;:";
-                    Date date=new Date();
-                    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-                    String time=sdf.format(date);
-                    try {
-                        date=sdf.parse(time);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    baseEnd=",&quot;time&quot;:"+date.getTime()+"}";
-                }
-                sp.edit().clear().commit();
-                SharedPreferences.Editor editor=sp.edit();
-                editor.putString("baseStep",baseHead+startCount+baseEnd);
-                editor.putBoolean("checkuser",true);
-                editor.putBoolean("startup",true);
-                editor.putString("last_stepinfo_today",head+count+end);
-                editor.putString("userId",userId);
-                editor.commit();
-            }
             process.waitFor();
             os.close();
             is.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        }finally {
+            if (os!=null)
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            process.destroy();
+        }
+        String cmd2="chmod 777 " + PCKAGE_NAME;
+        String editString2="cat /data/data/com.eg.android.AlipayGphone/shared_prefs/NewPedoMeter.xml";
+        try {
+            process = Runtime.getRuntime().exec("su"); //切换到root帐号
+            os = new DataOutputStream(process.getOutputStream());
+            is=new DataInputStream(process.getInputStream());
+            os.writeBytes(cmd2 + "\n");
+            os.writeBytes(editString2+"\n");
+            os.writeBytes("exit\n");
+            os.flush();
+            byte[] bs=new byte[8*1024*1024];
+            int len=-1;
+            len=is.read(bs);
+            File file=new File("/data/data/com.dzg.bootchange/shared_prefs/NewPedoMeter.xml");
+            FileWriter writer=new FileWriter(file);
+            if(len>0){
+                writer.write(new String(bs,0,len-1,"utf-8"));}
+            writer.flush();
+            writer.close();
+            process.waitFor();
+            os.close();
+            is.close();
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
+        }finally {
             if (os!=null)
                 try {
                     os.close();
@@ -192,54 +135,11 @@ public class MainActivity extends AppCompatActivity {
             process.destroy();
         }
     }
-    public void putToSP(){
-        String count= mEditText.getText().toString();
-        String startCount= mEditText.getText().toString();
-        SharedPreferences sp =getSharedPreferences("NewPedoMeter", Context.MODE_PRIVATE);
-        String userId=sp.getString("userId","0");
-        String boot=sp.getString("last_stepinfo_today","");
-        String head="";
-        String end="";
-        if (!boot.equals("")) {
-            head= boot.substring(0, boot.indexOf("steps\":") + "steps\":".length());
-            end= boot.substring(boot.lastIndexOf(","), boot.length());
-        }else
-        {
-            head="{&quot;biz&quot;:&quot;alipay&quot;,&quot;steps&quot;:";
-            end=",&quot;time&quot;:"+ new Date().getTime()+"}";
-        }
-        String baseStep=sp.getString("baseStep","");
-        String baseHead=null;
-        String baseEnd=null;
-        if(!baseStep.equals("")) {
-            baseHead= baseStep.substring(0, baseStep.indexOf(":") + 1);
-            baseEnd= baseStep.substring(baseStep.indexOf(","), baseStep.length());
-        }else{
-            baseHead=">{&quot;steps&quot;:";
-            Date date=new Date();
-            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-            String time=sdf.format(date);
-            try {
-                date=sdf.parse(time);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            baseEnd=",&quot;time&quot;:"+date.getTime()+"}";
-        }
-        sp.edit().clear().commit();
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putString("baseStep",baseHead+startCount+baseEnd);
-        editor.putBoolean("checkuser",true);
-        editor.putBoolean("startup",true);
-        editor.putString("last_stepinfo_today",head+count+end);
-        editor.putString("userId",userId);
-        editor.commit();
-    }
     public void Exec(String string){
         Process process = null;
         DataOutputStream os = null;
         String cmd="chmod 777 " + PCKAGE_NAME;
-        String editString="echo \""+string+"\">"+"/data/data/com.eg.android.AlipayGphone/shared_prefs/NewPedoMeter.xml";
+        String editString="echo \""+string+"\">"+"/data/data/com.eg.android.AlipayGphone/shared_prefs/NewPedoMeter_private.xml";
         try {
             process = Runtime.getRuntime().exec("su"); //切换到root帐号
             os = new DataOutputStream(process.getOutputStream());
@@ -255,5 +155,32 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } finally {
         }
+    }
+    public void editSP(){
+        SharedPreferences sp =getSharedPreferences("NewPedoMeter_private", Context.MODE_PRIVATE);
+        SharedPreferences sp2 =getSharedPreferences("NewPedoMeter", Context.MODE_PRIVATE);
+        String last_stepinfo=sp2.getString("last_stepinfo_today","");
+        String firstStep=sp.getString("firstStep","");
+        StepBean firstBean=  JSON.parseObject(firstStep.replaceAll("&quot;","\""),StepBean.class);
+        if (!last_stepinfo.equals("")){
+            StepBean lastBean=JSON.parseObject(last_stepinfo.replaceAll("&quot;","\""),StepBean.class);
+            firstBean.setTime(lastBean.getTime());
+        }
+        long timespan=new Date().getTime()-firstBean.getTime();
+        int step = (int) (timespan/1000);
+        int span=30;
+        List<StepBean> myList=new ArrayList<>();
+        Random random=new Random();
+        for (int i=0;i<step;i++){
+            StepBean stepBean=new StepBean();
+            i=i+random.nextInt(span);
+            stepBean.setSteps(firstBean.getSteps()+i*2);
+            stepBean.setTime(firstBean.getTime()+i*1000+random.nextInt(1000));
+            stepBean.setBiz(firstBean.getBiz());
+            myList.add(stepBean);
+        }
+        String jsonString=JSON.toJSONString(myList);
+        sp.edit().putString("stepRecord",jsonString).commit();
+        Log.e("getEBoot",jsonString);
     }
 }
--- app/src/test/java/com/dzg/bootchange/ExampleUnitTest.java
+++ app/src/test/java/com/dzg/bootchange/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.dzg.bootchange;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.dzg.bootchange.bean.StepBean;
+
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +20,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        String str="<string name=\"firstStep\">{&quot;biz&quot;:&quot;alipay&quot;,&quot;steps&quot;:0,&quot;time&quot;:1503261196824}</string>";
+        System.out.print(str.replace("\"","\\\""));
     }
 }
